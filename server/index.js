9d8cae665eba5143069ff177878e9f047b3949ac
Mike Flinn <mflinn92@gmail.com>
2019-10-29 10:47:10 -0400

    server/index.js now processes an image file into a raw Uint8array. this is then used with tf node to build a tensor which is then used to classify the image. Can now classify images using in tensorflow.js in both a browser and node environment

--- server/index.js
+++ server/index.js
@@ -1,1 +1,36 @@
 const tf = require('@tensorflow/tfjs-node');
+const mobileNet = require('@tensorflow-models/mobilenet');
+const jpeg = require('jpeg-js');
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+
+const readFile = util.promisify(fs.readFile);
+
+const getImgTensor = async (filePath) => {
+  let jpegData = await readFile(filePath);
+  let tensor = tf.node.decodeImage(jpegData);
+  return tensor
+}
+
+
+const Classify = async() => {
+  const model = await mobileNet.load();
+  const tensor = await getImgTensor('eagle.JPEG');
+  return await model.classify(tensor);
+
+}
+
+Classify()
+  .then(prediction => console.log(prediction))
+
+
+// const decodeImage = async(rawJpegData) => {
+//   return tf.node.decodeImage(rawJpegData);
+// }
+// getImgBuffer('eagle.JPEG')
+//   .then((imgData) => decodeImage(imgData.data))
+//   .then((tensor) => console.log(tensor));
+
+// const model = mobileNet.load()
+//   .then(console.log('model loaded'));
